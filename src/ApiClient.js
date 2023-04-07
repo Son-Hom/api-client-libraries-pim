b686b8d175c8812ed1a12ed873304f448f7abe84
Son HUM <son@USERs-MacBook-Pro.local>
2023-04-07 10:03:48 +0700

    Test change base path

--- src/ApiClient.js
+++ src/ApiClient.js
@@ -32,11 +32,11 @@ class ApiClient {
    * Overrides the default value set in spec file if present
    * @param {String} basePath
    */
-  constructor(basePath = "http://localhost:8080") {
+  constructor(basePath = "https://dev.aimlapps.com") {
     /**
      * The base URL against which to resolve every API call's (relative) path.
      * @type {String}
-     * @default http://localhost:8080
+     * @default https://dev.aimlapps.com
      */
     this.basePath = basePath.replace(/\/+$/, "");
 
